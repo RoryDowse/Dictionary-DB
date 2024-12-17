0c7502f9bfc49cc1aebe4e38304b71c4212d4d81
Rory Dowse <rorydowse@hotmail.com>
2024-12-16 19:33:23 -0700

    update vite.config with comments

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"; // import function to define and validate configuration options
+import react from "@vitejs/plugin-react"; // plugin to enable React and JSX/TSX support
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({ // configuration is exported to be used by Vite to apply setting
+  plugins: [react()], // adds React plugin
   build: {
-    outDir: "dist",
+    outDir: "dist", // specifies the output directory - running vite build will generate files in this directory
   },
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      "/graphql": {
-        target: "http://localhost:3001",
-        secure: false,
-        changeOrigin: true,
+  server: { // customizes the development server
+    port: 3000, // specifies the dev server port
+    open: true, // automatically opens the browser when the server starts
+    proxy: { // sets up proxy for API requests
+      "/graphql": { // redirects requests to the GraphQL endpoint
+        target: "http://localhost:3001", // specifies the target URL
+        secure: false, // allows requests to be sent to HTTP instead of HTTPS
+        changeOrigin: true, // changes the origin of the request
       },
     },
   },
